aec3ba5e53a227555cef500c2c4aa1fc8342d0a0
fanzhe <fanzhe@axzo.cn>
2024-04-06 10:21:06 +0800

    init paimon

--- flink-paimon-test/src/main/java/org/example/Main.java
+++ flink-paimon-test/src/main/java/org/example/Main.java
@@ -1,10 +1,11 @@
 package org.example;
 
+import com.fan.lakehouse.dw.ods2dwdDemo;
 import com.fan.lakehouse.read.ReadFromTable;
 import com.fan.lakehouse.write.WriteToTable;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ReadFromTable.readFrom();
+        ods2dwdDemo.writeToDwd();
     }
 }
